dd2276daff81df6e6fb61b5d8451524c87d314a5
jonathan nicholson <jonnicholson90@gmail.com>
2016-02-03 21:36:02 -0500

    use simpler annotation
    Use @SpringBootApplication instead of the three granular ones.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,13 +1,12 @@
 package calculator;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
+/**
+ * Main application class for launching the calculator.
+ */
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
