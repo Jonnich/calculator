e96150ad7b29c21840863766729b6ab0bfeb7fb3
jonathan nicholson <jonnicholson90@gmail.com>
2016-02-05 17:35:34 -0500

    remove operations handled be operations controller

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,14 +1,8 @@
 package calculator.controller;
 
-import calculator.service.OperationService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * The main controller for the calculator application.
@@ -17,18 +11,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("calculator")
 @Controller
 public class CalculatorController {
-
-    @Autowired
-    public OperationService operationService;
-
     @RequestMapping(method = RequestMethod.GET)
     public String calculator() {
         return "pages/calc.html";
-    }
-
-    @RequestMapping("/operation/{operation}/{firstOperand}/{secondOperand}/")
-    @ResponseBody
-    public String operation(@PathVariable String operation, @PathVariable float firstOperand, @PathVariable float secondOperand) {
-        return String.valueOf(operationService.doOperation(firstOperand, secondOperand, operation));
     }
 }
