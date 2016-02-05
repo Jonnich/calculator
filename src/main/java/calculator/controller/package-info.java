269bb36293f9c847bc498e5b8a69b15baa7141b0
jonathan nicholson <jonnicholson90@gmail.com>
2016-02-05 17:34:34 -0500

    package-info and documentation

--- src/main/java/calculator/controller/package-info.java
+++ src/main/java/calculator/controller/package-info.java
@@ -1,4 +1,5 @@
 /**
+ * Contains the controllers for the calculator application.
  * Created by jonathan on 2/2/16.
  */
 package calculator.controller;
---
+++ src/main/java/calculator/service/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Contains the service layer logic for the calculator.
+ * Created by jonathan on 2/5/16.
+ */
+package calculator.service;
