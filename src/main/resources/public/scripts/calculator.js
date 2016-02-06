49221a667c7bf3f52a17f7757ba4fe02444ae58b
jonathan nicholson <jonnicholson90@gmail.com>
2016-02-05 23:52:25 -0500

    organize all the javascript into methods

--- src/main/resources/public/scripts/calculator.js
+++ src/main/resources/public/scripts/calculator.js
@@ -1,7 +1,7 @@
 /**
+ * Handles the calculator operations.
  * Created by jonathan on 2/3/16.
  */
-
 $(
     function() {
 
@@ -9,74 +9,128 @@ $(
         var operandTwo = '';
         var operator;
 
+        /**
+         * Handles a user clicking an operand.
+         * @param whatWasClicked the id of the button being pressed
+         * @param $calcWindow a jquery object representation of the result window
+         */
+        function handleOperand(whatWasClicked, $calcWindow) {
+            if (!operator) {
+                if (!operandOne && whatWasClicked !== "0") {
+                    operandOne += whatWasClicked;
+                } else if (operandOne) {
+                    operandOne += whatWasClicked;
+                }
+                $calcWindow.val(operandOne);
+            } else {
+                if (!operandTwo && whatWasClicked !== "0") {
+                    operandTwo += whatWasClicked;
+                } else if (operandTwo) {
+                    operandTwo += whatWasClicked;
+                }
+                $calcWindow.val(operandTwo);
+            }
+        }
+
+        /**
+         * Handles a user clicking an operator.
+         * @param whatWasClicked the id of the button being pressed
+         * @param $calcWindow a jquery object representation of the result window
+         */
+        function handleOperator(whatWasClicked, $calcWindow) {
+            if (whatWasClicked === "-") {
+                if (!operandOne && !operator) {
+                    operandOne += whatWasClicked;
+                    $calcWindow.val(operandTwo);
+                } else if (!operandTwo && operator) {
+                    operandTwo += whatWasClicked;
+                    $calcWindow.val(operandTwo);
+                } else {
+                    operator = whatWasClicked;
+                }
+            } else {
+                operator = whatWasClicked;
+            }
+        }
+
+        /**
+         * Handles the clicking of the "." button.
+         * @param $calcWindow a jquery object representation of the result window
+         */
+        function handleDecimal($calcWindow) {
+            if (!operator) {
+                if (operandOne.indexOf(".") === -1) {
+                    operandOne += "."
+                }
+                $calcWindow.val(operandOne);
+            } else {
+                if (operandTwo.indexOf(".") === -1) {
+                    operandTwo += "."
+                }
+                $calcWindow.val(operandTwo);
+            }
+        }
+
+        /**
+         * Resets the state of the calculator.
+         * @param $calcWindow a jquery object representation of the result window
+         */
+        function resetCalculatorState($calcWindow) {
+            $calcWindow.val('');
+            operandOne = '';
+            operandTwo = '';
+            operator = null;
+        }
+
+        /**
+         * Handles the actions initiated by clicking the "=" button.
+         * @param $calcWindow a jquery object representation of the result window
+         */
+        function handleOperation($calcWindow) {
+            if (operandOne && operandTwo && operator) {
+                var args = {
+                    url: "calculator/calculate",
+                    data: {
+                        operandOne: operandOne,
+                        operandTwo: operandTwo,
+                        operator: operator
+                    },
+                    dataType: "json",
+                    method: "POST",
+                    success: function (data) {
+                        $calcWindow.val(data);
+                    }
+                };
+
+                $.ajax(args);
+
+                resetCalculatorState($calcWindow);
+            } else {
+                resetCalculatorState($calcWindow);
+            }
+        }
+
+        /**
+         * Binds the click event of the calculator form.
+         */
         $("#calculatorForm").click(
             function(evt) {
                 var $calcWindow = $("#calcWindow");
                 var whatWasClicked = evt.target.id;
                 var $whatWasClicked = $(evt.target);
 
-                // validate that the input window does not contain multiple .
-                // when an operator (other than =) is clicked, store value currently in window to operandOne
-                // when = is pressed call doOperation to send REST request to java to perform operation
-                // update calcWindow with returned JSON
-
                 if (whatWasClicked != 'calcWindow') {
                     if ($whatWasClicked.hasClass("operand")) {
-                        if (!operator) {
-                            operandOne += whatWasClicked;
-                            $calcWindow.val(operandOne);
-                        } else {
-                            operandTwo += whatWasClicked;
-                            $calcWindow.val(operandTwo);
-                        }
+                        handleOperand(whatWasClicked, $calcWindow);
                     } else if ($whatWasClicked.hasClass("operator")) {
-                        operator = whatWasClicked;
+                        handleOperator(whatWasClicked, $calcWindow);
                     } else if (whatWasClicked === ".") {
-                        if (!operator) {
-                            if (operandOne.indexOf(".") === -1) {
-                                operandOne += "."
-                            }
-                            $calcWindow.val(operandOne);
-                        } else {
-                            if (operandTwo.indexOf(".") === -1) {
-                                operandTwo += "."
-                            }
-                            $calcWindow.val(operandTwo);
-                        }
+                        handleDecimal($calcWindow);
                     } else if (whatWasClicked === "=") {
-                        if (operandOne && operandTwo && operator) {
-                            var args = {
-                                url : "calculator/calculate",
-                                data: {
-                                    operandOne : operandOne,
-                                    operandTwo : operandTwo,
-                                    operator : operator},
-                                dataType : "json",
-                                method : "POST",
-                                success : function(data) {
-                                    $calcWindow.val(data);
-                                },
-                                error : function() {
-                                    $calcWindow.error("No dice.");
-                                }
-                            };
-
-                            $.ajax(args);
-
-                            operandOne = '';
-                            operandTwo = '';
-                            operator = null;
-                        }
+                        handleOperation($calcWindow);
                     }
                 }
-            });
+            }
+        );
     }
 );
-
-var doOperation = function(firstOperand, secondOperand, operator) {
-
-};
-
-var pressNumber = function() {
-
-};
